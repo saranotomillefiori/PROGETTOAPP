c0e1189126797e7eb59a19fd9bb625e5dcfb5fa7
lawruea <la-wruea-@hotmail.it>
2023-05-22 09:26:23 +0200

    Modifica grafico

---
+++ app/src/main/java/com/example/progetto/MainActivity3.java
@@ -0,0 +1,31 @@
+package com.example.progetto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+
+        // aggiungo qui dei valori per il grafico 1 la view
+        GraphView graph = (GraphView) findViewById(R.id.Graphview1);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+    }
+}
