5c31f4cad0827846bce7f600f3e268139eb09954
lawruea <la-wruea-@hotmail.it>
2023-05-22 09:06:32 +0200

    Revert "q3ga"
    This reverts commit c5cb33555792b8d492e207a0525bdbec1ac50209, reversing
    changes made to e724bd6c6abe80697e4ada60bf032f82024057d0.

--- app/src/main/java/com/example/progetto/MainActivity.java
+++ app/src/main/java/com/example/progetto/MainActivity.java
@@ -2,43 +2,13 @@ package com.example.progetto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 
 public class MainActivity extends AppCompatActivity {
-//cambia nome al bottone qui:
-Button startbutton;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //BOTTONE trova il bottone//
-        startbutton = findViewById(R.id.startbutton);
-
-        // BOTTONE imposta azione quando il bottone viene cliccato tutto quello da qui in poi
-        startbutton.setOnClickListener(new View.OnClickListener() {
-     @Override
-    public void onClick(View v) {
-    Intent intent = new Intent(MainActivity.this, MainActivity3.class);
-    startActivity(intent);
     }
-        });
-    // fino a qui ancora bottone
-        }
-    }
-
-
-
-
-
-
-
+}
