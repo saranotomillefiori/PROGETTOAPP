8f2c6a2bfa7815ff61d09aaa21ffbca8fecb2e70
lawruea <la-wruea-@hotmail.it>
2023-05-22 10:22:14 +0200

    Nuovo tipdi grafico

--- app/src/main/java/com/example/progetto/MainActivity3.java
+++ app/src/main/java/com/example/progetto/MainActivity3.java
@@ -2,11 +2,19 @@ package com.example.progetto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity3 extends AppCompatActivity {
@@ -16,7 +24,24 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        LineChart chart = (LineChart) findViewById(R.id.chart);
 
+        List<Entry> entries = new ArrayList<Entry>();
+        entries.add(new Entry(0,1));
+        entries.add(new Entry(1,5));
+        entries.add(new Entry(2,3));
+        entries.add(new Entry(3,2));
+        entries.add(new Entry(4,6));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Test data"); // add entries to dataset
+        dataSet.setColor(Color.RED);
+        dataSet.setValueTextColor(Color.BLACK);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
+
+        /*
         // aggiungo qui dei valori per il grafico 1 la view
         GraphView graph = (GraphView) findViewById(R.id.Graphview1);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
@@ -26,6 +51,6 @@ public class MainActivity3 extends AppCompatActivity {
                 new DataPoint(3, 2),
                 new DataPoint(4, 6)
         });
-        graph.addSeries(series);
+        graph.addSeries(series);*/
     }
 }
