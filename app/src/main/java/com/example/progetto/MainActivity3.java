b45c26ec115a484d073bf941ba85e8904e856212
lawruea <la-wruea-@hotmail.it>
2023-05-22 12:02:58 +0200

    3 charts

--- app/src/main/java/com/example/progetto/MainActivity3.java
+++ app/src/main/java/com/example/progetto/MainActivity3.java
@@ -33,13 +33,50 @@ public class MainActivity3 extends AppCompatActivity {
         entries.add(new Entry(3,2));
         entries.add(new Entry(4,6));
 
-        LineDataSet dataSet = new LineDataSet(entries, "Test data"); // add entries to dataset
+        LineDataSet dataSet = new LineDataSet(entries, "Volume / Time"); // add entries to dataset
         dataSet.setColor(Color.RED);
         dataSet.setValueTextColor(Color.BLACK);
 
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
         chart.invalidate();
+
+        // seconda
+        LineChart chart2 = (LineChart) findViewById(R.id.chart2);
+
+        List<Entry> entries2 = new ArrayList<Entry>();
+        entries2.add(new Entry(0,1));
+        entries2.add(new Entry(1,5));
+        entries2.add(new Entry(2,3));
+        entries2.add(new Entry(3,2));
+        entries2.add(new Entry(4,6));
+
+        LineDataSet dataSet2 = new LineDataSet(entries2, "Pressure / Time"); // add entries to dataset
+        dataSet2.setColor(Color.GREEN);
+        dataSet2.setValueTextColor(Color.BLACK);
+
+        LineData lineData2 = new LineData(dataSet2);
+        chart2.setData(lineData2);
+        chart2.invalidate();
+
+        // terza
+
+        LineChart chart3 = (LineChart) findViewById(R.id.chart3);
+
+        List<Entry> entries3 = new ArrayList<Entry>();
+        entries3.add(new Entry(0,1));
+        entries3.add(new Entry(1,5));
+        entries3.add(new Entry(2,3));
+        entries3.add(new Entry(3,2));
+        entries3.add(new Entry(4,6));
+
+        LineDataSet dataSet3 = new LineDataSet(entries3, "AirFlow / Time"); // add entries to dataset
+        dataSet3.setColor(Color.BLUE);
+        dataSet3.setValueTextColor(Color.BLACK);
+
+        LineData lineData3 = new LineData(dataSet3);
+        chart3.setData(lineData3);
+        chart3.invalidate();
 
         /*
         // aggiungo qui dei valori per il grafico 1 la view
