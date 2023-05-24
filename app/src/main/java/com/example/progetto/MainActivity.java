b88e6c24223c520032ff372e8cd9a1e1269a5d06
lawruea <la-wruea-@hotmail.it>
2023-05-24 12:33:38 +0200

    infoPopUp

--- app/src/main/java/com/example/progetto/MainActivity.java
+++ app/src/main/java/com/example/progetto/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.progetto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.FloatingWindow;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
 //cambia nome al bottone qui:
 Button startbutton;
-
+Button buttoninfo;
+FloatingActionButton information;
 
 
 
@@ -33,8 +43,30 @@ Button startbutton;
     }
         });
     // fino a qui ancora bottone
-        }
-    }
+
+
+// BOTTONE INFO
+
+    //BOTTONE trova il bottone//
+   information = findViewById(R.id.information);
+   information.bringToFront();
+    // BOTTONE imposta azione quando il bottone viene cliccato tutto quello da qui in poi
+        information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+information= (FloatingActionButton) findViewById(R.id.information);
+               Intent i = new Intent(getApplicationContext(), PopActivity.class);
+                startActivity(i);
+            }
+        });
+
+}
+
+}
+
+
+
 
 
 
